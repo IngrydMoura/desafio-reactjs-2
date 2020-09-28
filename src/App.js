2b4c5e84857ca1e2b0e101710fa75fee65762504
ingryd.moura <ingryd.moura@corp.globo.com>
2020-09-28 20:25:59 -0300

    Renderiza repositorios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
   }
@@ -13,14 +22,14 @@ function App() {
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list"> 
+        {repositories.map(repository => 
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
